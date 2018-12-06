ebfd765a8e1ecc2accdc8217904b53e400296093
Mona Ka <monaka@outlook.com>
2018-12-06 15:13:42 -0600

    Updated index js & css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Calculator from './components/calculator.js';
+import './index.css';
 
 //Render app to page
 ReactDom.render( < Calculator / > , document.getElementById('app'));
