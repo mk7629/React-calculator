696ee8de526e87f1a17905c6fcbd0d74d3051cdb
Mona Ka <monaka@outlook.com>
2018-12-11 16:21:25 -0600

    Updated value

--- src/components/display.js
+++ src/components/display.js
@@ -1,20 +1,16 @@
 //Import React
-import React from 'react';
+import React from "react";
 //Import Display Row
-import DisplayRow from './displayRow.js';
+import DisplayRow from "./displayRow.js";
 
 //hold two display rows
-const Display = (props) => {
-    return ( <
-        div className = "display" >
-        <
-        DisplayRow value = { props.question }
-        / > <
-        DisplayRow value = { props.answer }
-        / > < /
-        div >
-    );
-}
+const Display = props => {
+	return (
+		<div className="display" id="display">
+			<DisplayRow value={props.question} /> <DisplayRow value={props.answer} />
+		</div>
+	);
+};
 
 //Export Display
 export default Display;
